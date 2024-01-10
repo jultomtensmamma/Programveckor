894c99460f4ccb6a753dc3c328fade6f89dbd4c7
MaryamMMKB <154224825+MaryamMMKB@users.noreply.github.com>
2024-01-10 13:29:49 +0100

    text test

---
+++ Assets/text.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using UnityEngine;
+
+public class text : MonoBehaviour
+{
+   public GameObject Text;
+    public GameObject TBO;
+
+    void Start()
+       
+    {
+       
+    }
+
+
+    void Update()
+    {
+        
+        
+
+    }
+}
