60239a9a2222cf458267e24e11382c9c8ee48fb2
Hampus CS <100205514+Hampe007@users.noreply.github.com>
2024-01-10 19:25:07 +0100

    Bug fixed
    Fixed missing files, removed merge conflicts, added credits, other fixes.

---
+++ Assets/Scripts/mainmenu.cs
@@ -0,0 +1,19 @@
+//Code by HCS
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainmenu : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("Player has quit the game");
+    }
+
+}
