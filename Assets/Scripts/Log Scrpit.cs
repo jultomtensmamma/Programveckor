90ab03f4ac0adf6ddd6ca182053b55f13b304e73
MaryamMMKB <154224825+MaryamMMKB@users.noreply.github.com>
2024-01-17 22:32:40 +0100

    r

--- Assets/Scripts/Log Scrpit.cs
+++ Assets/Scripts/Log Scrpit.cs
@@ -1,19 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.VersionControl;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LogScrpit : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private bool isPlayerInside;
+   
+    
+
+    private void OnTriggerEnter2D(Collider2D collider)
     {
         
-    }
-
-    private void OnTriggerExit2D(Collider2D collider)
-    {
-        SceneManager.LoadScene("Log");
+            if (gameObject.CompareTag("deer sprite walking spreadsheet_0"))
+            {
+                isPlayerInside = true;
+               SceneManager.LoadScene("GameScene");
+            }
+        
     }
 }
