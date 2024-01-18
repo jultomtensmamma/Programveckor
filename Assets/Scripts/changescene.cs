0f98fae4cad5f69bf15fb235dc94afd94000189d
MiraSkold <mira.skold@elev.ga.lbs.se>
2024-01-18 17:33:10 +0100

    summary
    summary

--- Assets/Scripts/changescene.cs
+++ Assets/Scripts/changescene.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class changescene : MonoBehaviour
 {
+    public string scene;
+    public void LoadGame()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            SceneManager.LoadScene(scene);
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
