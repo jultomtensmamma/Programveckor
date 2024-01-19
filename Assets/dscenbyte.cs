d68ac501fc27b3aa361036fe8e4e2d4f8a473e32
jultomtensmamma <julia.tillberg@elev.ga.lbs.se>
2024-01-19 14:16:08 +0100

    update

--- Assets/dscenbyte.cs
+++ Assets/dscenbyte.cs
@@ -1,8 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics;
-using Unity.VisualScripting;
-using UnityEditor.VersionControl;
+
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Debug = UnityEngine.Debug;
