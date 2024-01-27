1d12ca5a789808615e214fb815ba005b3f8ebe8b
MiraSkold <mira.skold@elev.ga.lbs.se>
2024-01-27 16:11:56 +0100

    summary

---
+++ Assets/inventory/objectscript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName ="Item/Create")] 
+public class objectscript : MonoBehaviour
+{
+    public int id;
+    public string itemName;
+    public int value;
+    public Sprite icon;
+
+
+}
